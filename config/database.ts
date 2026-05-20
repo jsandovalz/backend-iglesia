dfdce5b50909d80a63d68d7e8f399fb56ec4a501
Jose Sandoval <jsandovalz@gmail.com>
2026-05-20 10:40:51 -0400

    parser the DATABASE_URL

--- config/database.ts
+++ config/database.ts
@@ -1,17 +1,25 @@
 import type { Core } from '@strapi/strapi';
 
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Database => ({
-  connection: {
-    client: 'postgres',
+const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Database => {
+  const url = new URL(env('DATABASE_URL'));
+
+  return {
     connection: {
-      connectionString: env('DATABASE_URL'),
-      ssl: false,
+      client: 'postgres',
+      connection: {
+        host: url.hostname,
+        port: Number(url.port),
+        database: url.pathname.replace('/', ''),
+        user: url.username,
+        password: url.password,
+        ssl: false,
+      },
+      pool: {
+        min: 2,
+        max: 10,
+      },
     },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-  },
-});
+  };
+};
 
 export default config;
